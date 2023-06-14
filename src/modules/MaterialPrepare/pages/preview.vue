3f2f1bb772bfa62b87e74a597ed65725f7147757
pdsuwwz <pdsu.wwz@foxmail.com>
2023-06-14 18:56:43 +0800

    refact: 初步完成路由 资料管理-资料准备

---
+++ src/modules/MaterialPrepare/pages/preview.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-16px">
+    <h1>{{ pageTitle }}</h1>
+    <n-input v-model:value="inputValue" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+/**
+ * MaterialPreparePreview 成员管理-资料信息查看
+ */
+defineOptions({
+  name: 'MaterialPreparePreview'
+})
+
+const route = useRoute()
+const router = useRouter()
+
+const pageTitle = ref('MaterialPreparePreview 成员管理-资料信息查看')
+
+const inputValue = ref(pageTitle.value)
+
+
+</script>
+
+<style lang="scss" scoped>
+</style>
