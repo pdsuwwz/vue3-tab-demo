a9a75407d689eb963698bfe91ee8c1d43ab8261a
pdsuwwz <pdsu.wwz@foxmail.com>
2023-06-15 10:18:12 +0800

    refact: 初步完成路由 成本核算管理-分配和归集

---
+++ src/modules/CostDistribute/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-16px">
+    <h1>{{ pageTitle }}</h1>
+    <n-input v-model:value="inputValue" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+/**
+ * CostDistributeIndex 成本核算管理-成本数据列表
+ */
+defineOptions({
+  name: 'CostDistributeIndex'
+})
+
+const route = useRoute()
+const router = useRouter()
+
+const pageTitle = ref('CostDistributeIndex 成本核算管理-成本数据列表')
+
+const inputValue = ref(pageTitle.value)
+
+
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/modules/CostDistribute/store/index.ts
@@ -0,0 +1,21 @@
+import { defineStore } from 'pinia'
+
+import { sleep } from '@/utils/request'
+import * as CostDistributeAPI from '../api'
+
+export const useProjectStore = defineStore('CostDistribute', {
+  state: () => {
+    return {
+      demoList: {}
+    }
+  },
+  getters: {
+  },
+  actions: {
+    async createProject (query) {
+      await sleep(200)
+      const res = await CostDistributeAPI.createProject(query)
+      return this.filterResponse(res)
+    }
+  }
+})
