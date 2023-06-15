8ea9f5ab5ee0db4d672cf87169617eb72f9c4bea
pdsuwwz <pdsu.wwz@foxmail.com>
2023-06-15 10:14:19 +0800

    refact: 初步完成路由 成本核算管理-数据收集

---
+++ src/modules/CostCollection/pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-16px">
+    <h1>{{ pageTitle }}</h1>
+    <n-input v-model:value="inputValue" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+/**
+ * CostCollectionIndex 成本核算管理-数据收集预览
+ */
+defineOptions({
+  name: 'CostCollectionIndex'
+})
+
+const route = useRoute()
+const router = useRouter()
+
+const pageTitle = ref('CostCollectionIndex 成本核算管理-数据收集预览')
+
+const inputValue = ref(pageTitle.value)
+
+
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/modules/CostCollection/store/index.ts
@@ -0,0 +1,21 @@
+import { defineStore } from 'pinia'
+
+import { sleep } from '@/utils/request'
+import * as CostCollectionAPI from '../api'
+
+export const useProjectStore = defineStore('CostCollection', {
+  state: () => {
+    return {
+      demoList: {}
+    }
+  },
+  getters: {
+  },
+  actions: {
+    async createProject (query) {
+      await sleep(200)
+      const res = await CostCollectionAPI.createProject(query)
+      return this.filterResponse(res)
+    }
+  }
+})
