e824997f214c31c6658e8cbbf24bc76b4b47f7b3
pdsuwwz <pdsu.wwz@foxmail.com>
2023-06-26 18:44:53 +0800

    feat: update member role

--- src/modules/MemberTeam/data/index.ts
+++ src/modules/MemberTeam/data/index.ts
@@ -1,5 +1,5 @@
 // 随机获取数组中某一项
-function getRandomValueFromArray (arr: any[]) {
+function getRandomValueFromArray (arr: Array<any>) {
   const randomIndex = Math.floor(Math.random() * arr.length)
   return arr[randomIndex]
 }
@@ -23,20 +23,65 @@ export const userRankMap = [
   }
 ]
 
+// 角色映射表
+export const userRoleMap = [
+  {
+    key: 'team_member',
+    label: '团队成员'
+  },
+  {
+    key: 'team_leader',
+    label: '现场负责人'
+  },
+  {
+    key: 'project_manager',
+    label: '项目经理'
+  },
+  {
+    key: 'quality_manager',
+    label: '质控经理'
+  },
+  {
+    key: 'review_manager',
+    label: '复核经理'
+  }
+]
+
+/**
+ * 查询目标级别映射
+ */
+export const findUserRankMapByRankName = (targetRank) => {
+  return userRankMap.find(
+    rankItem => rankItem.key === targetRank
+  )!
+}
+
+/**
+ * 查询目标角色映射
+ */
+export const findUserRoleMapByRankName = (targetRole) => {
+  return userRoleMap.find(
+    roleItem => roleItem.key === targetRole
+  )!
+}
+
 export interface TypeMemberPerson {
-  key: string | number
   username: string
+  roleId: string
   userId: string
+  email: string
   rank: string
 }
 
 export const memberTeamList = Array.from({ length: 100 }).map((_, index) => {
   const userId = `BJ${(index + 1 + '').padStart(4, '0')}`
   const rankItem = getRandomValueFromArray(userRankMap)
+  const roleItem = getRandomValueFromArray(userRoleMap)
   return {
-    key: index,
     username: `Tom${index+1}`,
     userId,
-    rank: rankItem.key
+    roleId: roleItem.key,
+    rank: rankItem.key,
+    email: `${userId}@admin.com`
   } as TypeMemberPerson
 })
--- src/modules/MemberTeam/pages/preview.vue
+++ src/modules/MemberTeam/pages/preview.vue
@@ -2,10 +2,32 @@
   <div class="p-16px">
     <h1>{{ pageTitle }}</h1>
     <n-input v-model:value="inputValue" />
+
+
+    <n-form
+      ref="refForm"
+      :model="memberModelData"
+      label-placement="top"
+      label-width="auto"
+    >
+      <n-form-item
+        v-for="(infoItem, index) in memberInfoMap"
+        :key="index"
+        :path="infoItem.path"
+        :label="infoItem.label"
+      >
+        <n-input
+          v-model:value="memberModelData[infoItem.path]"
+        />
+      </n-form-item>
+    </n-form>
   </div>
 </template>
 
 <script lang="ts" setup>
+import type { FormInst } from 'naive-ui'
+import { TypeMemberPerson } from '../data'
+
 
 /**
  * MemberTeamPreview 成员管理-成员信息查看
@@ -20,6 +42,53 @@ const router = useRouter()
 const pageTitle = ref('MemberTeamPreview 成员管理-成员信息查看')
 
 const inputValue = ref(pageTitle.value)
+
+
+const memberModelData = reactive<TypeMemberPerson>({
+  username: 's',
+  roleId: 's',
+  userId: 's',
+  email: 'sss',
+  rank: 'ssssss'
+})
+
+const memberInfoMap = [
+  {
+    path: 'username',
+    label: 'username'
+  },
+  {
+    path: 'roleId',
+    label: 'roleId'
+  },
+  {
+    path: 'userId',
+    label: 'userId'
+  },
+  {
+    path: 'email',
+    label: 'email'
+  },
+  {
+    path: 'rank',
+    label: 'rank'
+  }
+]
+
+const refForm = ref<FormInst>()
+
+const validateRules = async () => {
+  return new Promise((resolve) => {
+    refForm.value!.validate(errors => {
+      if (errors) {
+        resolve(false)
+      } else {
+        resolve(true)
+      }
+    })
+  })
+}
+
 
 
 </script>
