1747be86b2e24edd38d46b49efc6881c1e6f7458
Wisdom <pdsu.wwz@foxmail.com>
2024-01-19 13:56:28 +0800

    🐣 fix: hmr hot reload circular import (#53)

--- src/store/plugins/externalProperties.ts
+++ src/store/plugins/externalProperties.ts
@@ -1,7 +1,5 @@
 import { getFilterResponse } from '@/store/utils/mixin'
-import router from '@/router'
 
 export const pluginExternalProperties = ({ store }) => {
   store.filterResponse = getFilterResponse
-  store.router = router
 }
